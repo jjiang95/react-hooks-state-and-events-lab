55e0ba675889769a9c43fd6c2ba24f5cced299b8
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-05-24 19:04:33 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  const [isAdded, setAdded] = useState(false)
+
+  function handleClick() {
+    setAdded((isAdded) => !isAdded)
+  }
+
   return (
-    <li className="">
+    <li className={isAdded ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={isAdded ? "remove" : "add"}>{isAdded ? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setCategory] = useState("All");
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value)
+  }
+
+  const itemList = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  })
+  
+  function renderedItems(itemList) {
+    return (
+      itemList.map((item) => {
+        return <Item key={item.id} name={item.name} category={item.category} />
+      })
+    )
+  }
+
+  const renderedList = renderedItems(itemList);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +38,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {renderedList}
       </ul>
     </div>
   );
